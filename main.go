301a57e6c83fa1f4867ea2f5efa82bed05c70585
Ramiro Ramos <ramirorj68@gmail.com>
2023-12-28 15:34:28 +0100

    Iniciando templates

--- main.go
+++ main.go
@@ -17,6 +17,18 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a "+"href=\"mailto:jon@calhoun.io\">jon@calhoun.io</a>.")
 }
 
+func contactIdHandler(w http.ResponseWriter, r *http.Request) {
+	contactID := chi.URLParam(r, "contactID")
+
+	ctx := r.Context()
+	key, ok := ctx.Value("key").(string)
+	if !ok {
+		key = "clave no definida"
+	}
+
+	w.Write([]byte(fmt.Sprintf("Hi %v, %v", contactID, key)))
+}
+
 func faqHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, `<h2>Q: Is there a free versión</h2>
@@ -27,6 +39,7 @@ func main() {
 	r := chi.NewRouter()
 	r.Get("/", homeHandler)
 	r.Get("/contact", contactHandler)
+	r.Get("/contact/{contactID}", contactIdHandler)
 	r.Get("/faq", faqHandler)
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page Not Found", http.StatusNotFound)
