cedef473cebfa29769d745748576b71e0d96159b
Ramiro Ramos <ramirorj68@gmail.com>
2024-01-07 19:15:32 +0100

    Embedded our templates and added support for parsing embedded templates

--- main.go
+++ main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/rm2formaciondigital/go-web-course/controllers"
+	"github.com/rm2formaciondigital/go-web-course/templates"
 	"github.com/rm2formaciondigital/go-web-course/views"
 )
 
@@ -14,16 +14,16 @@ func main() {
 	r := chi.NewRouter()
 
 	r.Get("/", controllers.StaticHandler(
-		views.Must(views.Parse(filepath.Join("templates", "home.gotmpl")))))
+		views.Must(views.ParseFS(templates.FS, "home.gotmpl"))))
 
 	r.Get("/contact", controllers.StaticHandler(
-		views.Must(views.Parse(filepath.Join("templates", "contact.gotmpl")))))
+		views.Must(views.ParseFS(templates.FS, "contact.gotmpl"))))
 
 	r.Get("/faq", controllers.StaticHandler(
-		views.Must(views.Parse(filepath.Join("templates", "faq.gotmpl")))))
+		views.Must(views.ParseFS(templates.FS, "faq.gotmpl"))))
 
 	r.Get("/support", controllers.StaticHandler(
-		views.Must(views.Parse(filepath.Join("templates", "support.gotmpl")))))
+		views.Must(views.ParseFS(templates.FS, "support.gotmpl"))))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page Not Found", http.StatusNotFound)
