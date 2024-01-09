0f7fae3e72dd1fad424b752858f7a41c6c3bd46c
Ramiro Ramos <ramirorj68@gmail.com>
2024-01-09 13:31:27 +0100

    FAQ page dinamic with templates

--- controllers/static.go
+++ controllers/static.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"html/template"
 	"net/http"
 
 	"github.com/rm2formaciondigital/go-web-course/views"
@@ -9,5 +10,28 @@ import (
 func StaticHandler(tpl views.Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		tpl.Execute(w, nil)
+	}
+}
+
+func FAQ(tpl views.Template) http.HandlerFunc {
+	questions := []struct {
+		Question string
+		Answer   template.HTML
+	}{
+		{
+			Question: "there a free versión?",
+			Answer:   "Yes!, We Offer a free trial for 30 days on any paid plans.",
+		},
+		{
+			Question: "What are your support hours?",
+			Answer:   `We hace support staff answering emails 24/7, though response times may be a bit slower on weekends.`,
+		},
+		{
+			Question: "How do I contact support?",
+			Answer:   "Email us - support@lenslocked.com",
+		},
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, questions)
 	}
 }
